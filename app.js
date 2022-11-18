350fb5d0cfcfe610c8262575a4ce6f3963664679
Syafiatul Kamila <20082010016@student.upnjatim.ac.id>
2022-11-18 21:52:17 +0700

    crud beasiswa

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ import userRouter from "./src/router/user.router.js";
 import adminRouter from "./src/router/admin-auth.router.js";
 import adminLokerRouter from "./src/router/admin-loker.router.js";
 import userLokerRouter from "./src/router/user-loker.router.js";
+import adminBeasiswaRouter from "./src/router/beasiswa-admin.router.js"
+import userBeasiswaRouter from "./src/router/beasiswa-user.router.js"
 import { Redis } from "./src/config/redis.connect.js";
 import passp from "./src/middleware/passport.middleware.js";
 
@@ -31,6 +33,8 @@ app.use("/api/user/auth", userRouter);
 app.use("/api/admin/auth", adminRouter);
 app.use("/api/admin/loker", passport.authenticate("jwt-admin", { session: false }), adminLokerRouter);
 app.use("/api/user/loker", passport.authenticate("jwt-user", { session: false }), userLokerRouter);
+app.use("/api/admin/beasiswa", passport.authenticate("jwt-admin", {session: false }), adminBeasiswaRouter);
+app.use("/api/user/beasiswa", passport.authenticate("jwt-user", {session: false }), userBeasiswaRouter);
 
 /* TEST AUTH USER */
 app.get("/secret/user", passport.authenticate("jwt-user", { session: false }), (req, res, next) => {
